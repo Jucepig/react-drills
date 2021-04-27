38589f0750d727c30f2358b3faa66e226d1ab8e4
Joe Fatani <jucepig@gmail.com>
2021-04-26 23:27:29 -0600

    completed react drills 1-8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      magicCards : []
+    }
+  }
+
+  componentDidMount() {
+    axios
+    .get('https://api.magicthegathering.io/v1/cards')
+    .then(res => {
+      console.log(res.data.cards)
+      this.setState({
+        magicCards : res.data.cards
+      })
+    })
+    .catch (err => {
+      console.log('GET request failed err: ' + err)
+    })
+  }
+
+
+  render() {
+    const {magicCards} = this.state
+    return (
+      <div className="App">
+        {magicCards.map((card) => {
+          return(
+            <div>
+              <h3>{card.name}</h3>
+              <img src={card.imageUrl} />
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
